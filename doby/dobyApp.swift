3fa41e07adb4f82828114116a9d41ebbd172fea9
BonClay <shabelnikov06@gmail.com>
2026-02-14 22:30:00 +0300

    Initial Commit

---
+++ doby/dobyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dobyApp.swift
+//  doby
+//
+//  Created by Александр Шабельников on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct dobyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dobyTests/dobyTests.swift
@@ -0,0 +1,17 @@
+//
+//  dobyTests.swift
+//  dobyTests
+//
+//  Created by Александр Шабельников on 14.02.2026.
+//
+
+import Testing
+@testable import doby
+
+struct dobyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
